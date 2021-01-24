f1b9a64cd148e6b118b5da63cde1058622d4cd65
marinarismeno <marinarismeno@users.noreply.github.com>
2021-01-24 14:29:32 +0200

    all the project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "Pull the bird and send it to attack the monsters!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private string _nextLevelName;
+
+    Monster[] _monsters;
+
+    void OnEnable()
+    {
+        _monsters = FindObjectsOfType<Monster>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex > 0)
+            if (MonstersAreAllDead())
+                GoToNextLevel();
+    }
+
+    void GoToNextLevel()
+    {
+        //Debug.Log("Go to level " + _nextLevelName);
+        SceneManager.LoadScene(_nextLevelName);
+    }
+
+    bool MonstersAreAllDead()
+    {
+        foreach (var monster in _monsters)
+        {
+            if (monster.gameObject.activeSelf)
+                return false;
+        }
+        return true;
+    }
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void Exit()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+}
